981f52b1cc81055ef27d4fda0dc8b9008c23250c
Sourav Ganguly <souravganguly59@gmail.com>
2020-08-17 00:57:12 +0530

    Update secret.js

--- config/secret.js
+++ config/secret.js
@@ -1,5 +1,5 @@
 module.exports = {
-    uri: 'mongodb+srv://admin:admin321@cluster0-gvinm.mongodb.net/app?retryWrites=true&w=majority',
+    uri: 'mongodb+srv://uster0-gvinm.mongodb.net/app?retryWrites=true&w=majority',
     apiUrl:'api/v1/KE',
-    secret:'ThisIsARandomKey@Sourav'
+    secret:'ThisIsARandomKey'
 }
